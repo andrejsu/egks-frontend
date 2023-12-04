aa6a3820306e8652fb5502b20d8fcfc83b02f40c
Andrey Sukhikh <andrey.sukhikh@jetruby.com>
2023-12-04 13:37:28 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./template/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      'gulf-water': '#2EACC5',
+      'blue-nebula': '#1C9EFF',
+      'pink': '#F63868',
+      'green': '#00CE7C',
+      'yellow': '#FE8B0B',
+      'gray-main': '#616872',
+      'gray-secondary': '#AAB5CB',
+      'light-gray': '#ECF0F3',
+      'bianchi-green': '#43C8C5',
+      'clay': '#BDC8DE',
+      'smoke': '#D1D9E6',
+      'black-blue': '#000935',
+      'dark-blue': '#121E3B',
+      'gray': '#434344',
+      'black': '#181A27',
+      'black-input': '#1F212E',
+      'tooth': '#F3F6FE',
+      'white': '#fff'
+    }
+  },
+  plugins: [],
+}
+
